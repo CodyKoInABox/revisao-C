adb882f5bcc8068f2d554d950933ab8fd752a8c5
CodyKoInABox <codykoinabox@gmail.com>
2023-08-03 23:01:28 -0300

    questao 5

---
+++ 5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// 5) Escrever um programa que incremente uma variável com valor mínimo 0 e valor máximo 100 e as mostre no vídeo.
+
+int main(){
+    
+    for(int i = 0; i <= 100; i++){
+        printf("%d, ", i);
+    }
+
+    return 0;
+}
